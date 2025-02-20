03a7949bae95d562d431d98d9ff25e453c0e62f7
Developer-Elisha <developer.elisha@gmail.com>
2025-02-20 18:01:01 +0500

    server

--- index.js
+++ index.js
@@ -18,5 +18,7 @@ app.get('/', (req, res) => {
     res.send('API is running');
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
